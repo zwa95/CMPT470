44cd7f632d8e986aa53f7017d996e802c78d1875
Mark Wu <zwa95@outlook.com>
2020-03-22 01:50:18 -0700

    update

--- a34/public/javascripts/index.js
+++ a34/public/javascripts/index.js
@@ -9,7 +9,7 @@ function randomize() {
     document.getElementById("create-name").value = random_fname[f_index] + " " + random_lname[l_index];
     document.getElementById("create-email").value = random_fname[f_index] + "_" + random_lname[l_index] + random_email[email_index];
     document.getElementById("create-age").value = parseInt(Math.random() * 110);
-    document.getElementById("create-gpa").value = parseFloat((Math.random() * 433)/100).toFixed(2);
+    document.getElementById("create-gpa").value = parseFloat((Math.random() * 433) / 100).toFixed(2);
 }
 
 function list_users() {
@@ -17,16 +17,16 @@ function list_users() {
     $.ajax({
         type: 'POST',
         url: '/list',
-        success: function(resultData) {
-            result=resultData
+        success: function (resultData) {
+            result = resultData
         }
-    }).done(()=>{
+    }).done(() => {
         // console.log(result);
         return result;
     });
 }
 
-function refresh_users(){
+function refresh_users() {
     var table = document.getElementById("user_table");
     while (table.firstChild) {  // clear the table
         table.firstChild.remove();
@@ -35,12 +35,12 @@ function refresh_users(){
     $.ajax({
         type: 'POST',
         url: '/list',
-        success: function(resultData) {
-            result=resultData;
+        success: function (resultData) {
+            result = resultData;
         }
-    }).done(()=>{
+    }).done(() => {
         console.log(result);
-        result.forEach((element)=>{
+        result.forEach((element) => {
             var new_row = table.insertRow(-1);
             var uid_cell = new_row.insertCell(-1);
             var name_cell = new_row.insertCell(-1);
@@ -48,7 +48,7 @@ function refresh_users(){
             var age_cell = new_row.insertCell(-1);
             var gpa_cell = new_row.insertCell(-1);
             uid_cell.innerHTML = element.uid;
-            name_cell.innerHTML= element.name;
+            name_cell.innerHTML = element.name;
             email_cell.innerHTML = element.email;
             age_cell.innerHTML = element.age;
             gpa_cell.innerHTML = element.gpa;
@@ -56,167 +56,167 @@ function refresh_users(){
     });
 }
 
-function create_user(){
+function create_user() {
     var name = document.getElementById("create-name").value;
     var email = document.getElementById("create-email").value;
     var age = document.getElementById("create-age").value;
     var gpa = document.getElementById("create-gpa").value;
-    if(name!="" && email!="" && age!="" && gpa!=""){
-        var req_body = {"name":name,"email":email,"age":age,"gpa":gpa};
+    if (name != "" && email != "" && age != "" && gpa != "") {
+        var req_body = { "name": name, "email": email, "age": age, "gpa": gpa };
         console.log(req_body);
         $.ajax({
             type: "POST",
             url: "/create",
             data: req_body,
-            success: function( data, textStatus, jQxhr ){
+            success: function (data, textStatus, jQxhr) {
                 console.log(data[0]);
                 refresh_users();
             },
-          });
-    }else{
+        });
+    } else {
         alert("Please fill in all fields before creating");
     }
 }
 
-function update_user(){
+function update_user() {
     var uid = document.getElementById("update-uid").value;
     var name = document.getElementById("update-name").value;
     var email = document.getElementById("update-email").value;
     var age = document.getElementById("update-age").value;
     var gpa = document.getElementById("update-gpa").value;
-    if(uid!="" && name!="" && email!="" && age!="" && gpa!=""){
-        var req_body = {"uid":uid,"name":name,"email":email,"age":age,"gpa":gpa};
+    if (uid != "" && name != "" && email != "" && age != "" && gpa != "") {
+        var req_body = { "uid": uid, "name": name, "email": email, "age": age, "gpa": gpa };
         console.log(req_body);
         $.ajax({
             type: "POST",
             url: "/update",
             data: req_body,
-            success: function( data, status, xhr ){
+            success: function (data, status, xhr) {
                 console.log(data[0]);
                 refresh_users();
             },
-          });
-    }else{
+        });
+    } else {
         alert("Please fill in all fields before updating");
     }
 }
 
-function load_user(){
+function load_user() {
     var uid = document.getElementById("update-uid").value.trim();
 
-    if(uid!=""){
-        var req_body={"uid":uid};
+    if (uid != "") {
+        var req_body = { "uid": uid };
         $.ajax({
             type: "POST",
             url: "/search",
             data: req_body,
-            success: function( data, status, xhr ){
+            success: function (data, status, xhr) {
                 console.log(data[0]);
-                document.getElementById("update-name").value=data[0].name;
-                document.getElementById("update-email").value=data[0].email;
-                document.getElementById("update-age").value=data[0].age;
-                document.getElementById("update-gpa").value=data[0].gpa;
+                document.getElementById("update-name").value = data[0].name;
+                document.getElementById("update-email").value = data[0].email;
+                document.getElementById("update-age").value = data[0].age;
+                document.getElementById("update-gpa").value = data[0].gpa;
             },
-          });
-    }else{
+        });
+    } else {
         alert("Please enter User ID before searching");
     }
 }
 
-function delete_user(){
+function delete_user() {
     var uid = document.getElementById("delete-uid").value.trim();
-    if(uid!=""){
-        var req_body={"uid":uid};
+    if (uid != "") {
+        var req_body = { "uid": uid };
         $.ajax({
             type: "POST",
             url: "/delete",
             data: req_body,
-            success: function( data, status, xhr ){
+            success: function (data, status, xhr) {
                 console.log(`User ${uid} deleted`);
                 refresh_users();
             },
-          });
-    }else{
+        });
+    } else {
         alert("Please enter User ID before deleting");
     }
 }
 
-function show(){
+function show() {
     var result;
     $.ajax({
         type: 'POST',
         url: '/list',
-        success: function(resultData) {
-            result=resultData;
+        success: function (resultData) {
+            result = resultData;
         }
-    }).done(()=>{
+    }).done(() => {
         show_stat(result);
         draw_age(result);
         draw_gpa(result);
     });
 }
 
-function draw_age(result){
+function draw_age(result) {
     var age_visualization = document.getElementById("age_visualization");
     while (age_visualization.firstChild) {  // clear the box
         age_visualization.firstChild.remove();
     }
     var max_age = -1;
-    result.forEach((element)=>{
-        max_age = Math.max(max_age,element.age);
+    result.forEach((element) => {
+        max_age = Math.max(max_age, element.age);
     });
-    result.forEach((element)=>{
+    result.forEach((element) => {
         var br = document.createElement("br");
         var progress = document.createElement("div");
-        progress.setAttribute("class","progress")
+        progress.setAttribute("class", "progress")
         var progress_bar = document.createElement("div");
-        progress_bar.setAttribute("class","progress-bar");
-        progress_bar.setAttribute("role","progressbar");
-        var age_percentage = (element.age/max_age)*100;
-        progress_bar.setAttribute("style",`width:${age_percentage}%`);
-        progress_bar.innerHTML=element.name+": "+element.age;
+        progress_bar.setAttribute("class", "progress-bar");
+        progress_bar.setAttribute("role", "progressbar");
+        var age_percentage = (element.age / max_age) * 100;
+        progress_bar.setAttribute("style", `width:${age_percentage}%`);
+        progress_bar.innerHTML = element.name + ": " + element.age;
         progress.appendChild(progress_bar);
         age_visualization.appendChild(progress);
         age_visualization.appendChild(br);
     });
 }
 
-function draw_gpa(result){
+function draw_gpa(result) {
     var gpa_visualization = document.getElementById("gpa_visualization");
     while (gpa_visualization.firstChild) {  // clear the box
         gpa_visualization.firstChild.remove();
     }
     var max_gpa = -1;
-    result.forEach((element)=>{
-        max_gpa = Math.max(max_gpa,element.gpa);
+    result.forEach((element) => {
+        max_gpa = Math.max(max_gpa, element.gpa);
     });
-    result.forEach((element)=>{
+    result.forEach((element) => {
         var br = document.createElement("br");
         var progress = document.createElement("div");
-        progress.setAttribute("class","progress")
+        progress.setAttribute("class", "progress")
         var progress_bar = document.createElement("div");
-        progress_bar.setAttribute("class","progress-bar");
-        progress_bar.setAttribute("role","progressbar");
-        var gpa_percentage = (element.gpa/max_gpa)*100;
-        progress_bar.setAttribute("style",`width:${gpa_percentage}%`);
-        progress_bar.innerHTML=element.name+": "+element.gpa;
+        progress_bar.setAttribute("class", "progress-bar");
+        progress_bar.setAttribute("role", "progressbar");
+        var gpa_percentage = (element.gpa / max_gpa) * 100;
+        progress_bar.setAttribute("style", `width:${gpa_percentage}%`);
+        progress_bar.innerHTML = element.name + ": " + element.gpa;
         progress.appendChild(progress_bar);
         gpa_visualization.appendChild(progress);
         gpa_visualization.appendChild(br);
     });
 }
-function show_stat(result){
+function show_stat(result) {
     var total_users = result.length;
-    document.getElementById("stat-total").innerHTML=total_users;
-    var total_gpa=0;
-    var total_age=0;
-    result.forEach((elem)=>{
+    document.getElementById("stat-total").innerHTML = total_users;
+    var total_gpa = 0;
+    var total_age = 0;
+    result.forEach((elem) => {
         total_age += elem.age;
         total_gpa += elem.gpa;
     });
-    console.log(total_age+"|"+total_gpa);
-    var avg_gpa = parseFloat(total_gpa/total_users).toFixed(1);
-    var avg_age = parseFloat(total_age/total_users).toFixed(1);
-    document.getElementById("stat-gpa").innerHTML=avg_gpa;
-    document.getElementById("stat-age").innerHTML=avg_age;
+    console.log(total_age + "|" + total_gpa);
+    var avg_gpa = parseFloat(total_gpa / total_users).toFixed(1);
+    var avg_age = parseFloat(total_age / total_users).toFixed(1);
+    document.getElementById("stat-gpa").innerHTML = avg_gpa;
+    document.getElementById("stat-age").innerHTML = avg_age;
 }
--- a34/routes/index.js
+++ a34/routes/index.js
@@ -1,81 +1,81 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var pubpath =  path.join(__dirname + '/../public');
+var pubpath = path.join(__dirname + '/../public');
 var mysql = require('mysql');
 const uuid = require('uuid');
 var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database:'cmpt470'
+  host: '0.0.0.0',
+  user: 'remote',
+  password: 'remote',
+  database: 'cmpt470'
 });
 db.connect();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.sendFile(path.join(pubpath + '/index.html'));
 });
-router.get('/show', function(req, res, next) {
+router.get('/show', function (req, res, next) {
   res.sendFile(path.join(pubpath + '/show.html'));
 });
 
-router.post('/create',function(req,res){
-  var uid = '\''+uuid.v4()+'\''; // generate unique uid
-  var name = '\''+req.body.name+'\'';
-  var email = '\''+req.body.email+'\'';
+router.post('/create', function (req, res) {
+  var uid = '\'' + uuid.v4() + '\''; // generate unique uid
+  var name = '\'' + req.body.name + '\'';
+  var email = '\'' + req.body.email + '\'';
   var age = req.body.age;
   var gpa = req.body.gpa;
-  var query = "INSERT INTO users (uid,name,email,age,gpa) VALUES ("+uid+","+name+","+email+","+age+","+gpa+")";
+  var query = "INSERT INTO users (uid,name,email,age,gpa) VALUES (" + uid + "," + name + "," + email + "," + age + "," + gpa + ")";
   console.log(query);
-  db.query(query,function(err,result){
-    if(err) res.status(400).send(err);
+  db.query(query, function (err, result) {
+    if (err) res.status(400).send(err);
     console.log("User added");
     res.status(200).send(result);
   });
 });
 
-router.post('/list',function(req,res){
+router.post('/list', function (req, res) {
   var query = "SELECT * FROM users";
   console.log(query);
-  db.query(query,function(err,result){
-    if(err) throw err;
+  db.query(query, function (err, result) {
+    if (err) throw err;
     console.log(result);
     res.status(200).send(result);
   });
 });
 
-router.post('/search',function(req,res){
-  var uid = '\''+req.body.uid+'\'';
-  var query = "SELECT * FROM users WHERE uid = "+uid +";";
+router.post('/search', function (req, res) {
+  var uid = '\'' + req.body.uid + '\'';
+  var query = "SELECT * FROM users WHERE uid = " + uid + ";";
   console.log(query);
-  db.query(query,function(err,result){
-    if(err) res.status(400).send(err);;
+  db.query(query, function (err, result) {
+    if (err) res.status(400).send(err);;
     res.status(200).send(result);
   });
 });
 
-router.post('/update',function(req,res){
-  var uid = '\''+req.body.uid+'\'';
-  var name = '\''+req.body.name+'\'';
-  var email = '\''+req.body.email+'\'';
+router.post('/update', function (req, res) {
+  var uid = '\'' + req.body.uid + '\'';
+  var name = '\'' + req.body.name + '\'';
+  var email = '\'' + req.body.email + '\'';
   var age = req.body.age;
   var gpa = req.body.gpa;
   var query = `UPDATE users SET name = ${name},email=${email},age=${age},gpa=${gpa} WHERE uid=${uid}`;
   console.log(query);
-  db.query(query,function(err,result){
-    if(err) res.status(400).send(err);
+  db.query(query, function (err, result) {
+    if (err) res.status(400).send(err);
     console.log(result);
     res.status(200).send(result);
   });
 });
 
-router.post('/delete',function(req,res){
-  var uid = '\''+req.body.uid+'\'';
+router.post('/delete', function (req, res) {
+  var uid = '\'' + req.body.uid + '\'';
   var query = `DELETE FROM users WHERE uid = ${uid}`;
   console.log(query);
-  db.query(query,function(err,result){
-    if(err) throw err;
+  db.query(query, function (err, result) {
+    if (err) throw err;
     console.log(result);
     res.send(result);
   });
